ca9832ab71be18157554ba16f33e70d648e00060
mstfhlc <mustafahalici30@gmail.com>
2021-03-16 21:32:47 +0100

    creating

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myTrueBoolean(){
+
+    if(myTrueBoolean){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(true);
+
+// Only change code below this line
+module.exports=myTrueBoolean;
